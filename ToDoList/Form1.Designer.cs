3b65addcb77edef955d1f975a39bddaca82bc5f5
yabloko-luv <ibnasbtya@gmail.com>
2025-10-16 02:43:19 +1000

    First commit

---
+++ ToDoList/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace ToDoList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.descriptTextBox = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.toDoListView = new System.Windows.Forms.DataGridView();
+            this.deletebutton = new System.Windows.Forms.Button();
+            this.savebutton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.toDoListView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Location = new System.Drawing.Point(263, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(531, 70);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Mistral", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.textBox1.Location = new System.Drawing.Point(162, 20);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 33);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "TO DO LIST";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Font = new System.Drawing.Font("MS Reference Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.titleTextBox.Location = new System.Drawing.Point(18, 88);
+            this.titleTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(618, 23);
+            this.titleTextBox.TabIndex = 1;
+            // 
+            // descriptTextBox
+            // 
+            this.descriptTextBox.Font = new System.Drawing.Font("MS Reference Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.descriptTextBox.Location = new System.Drawing.Point(18, 146);
+            this.descriptTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.descriptTextBox.Name = "descriptTextBox";
+            this.descriptTextBox.Size = new System.Drawing.Size(618, 23);
+            this.descriptTextBox.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("MS Reference Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(18, 124);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(225, 16);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "Краткое описание";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Location = new System.Drawing.Point(18, 66);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(155, 16);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.Text = "Название задачи";
+            // 
+            // toDoListView
+            // 
+            this.toDoListView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.toDoListView.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.toDoListView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.toDoListView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.toDoListView.Location = new System.Drawing.Point(18, 185);
+            this.toDoListView.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.toDoListView.Name = "toDoListView";
+            this.toDoListView.RowHeadersWidth = 51;
+            this.toDoListView.RowTemplate.Height = 24;
+            this.toDoListView.Size = new System.Drawing.Size(776, 400);
+            this.toDoListView.TabIndex = 9;
+            // 
+            // deletebutton
+            // 
+            this.deletebutton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.deletebutton.Font = new System.Drawing.Font("MS Reference Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.deletebutton.Location = new System.Drawing.Point(644, 146);
+            this.deletebutton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.deletebutton.Name = "deletebutton";
+            this.deletebutton.Size = new System.Drawing.Size(150, 23);
+            this.deletebutton.TabIndex = 11;
+            this.deletebutton.Text = "delete";
+            this.deletebutton.UseVisualStyleBackColor = false;
+            this.deletebutton.Click += new System.EventHandler(this.deletebutton_Click);
+            // 
+            // savebutton
+            // 
+            this.savebutton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.savebutton.Font = new System.Drawing.Font("MS Reference Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.savebutton.Location = new System.Drawing.Point(644, 88);
+            this.savebutton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.savebutton.Name = "savebutton";
+            this.savebutton.Size = new System.Drawing.Size(150, 23);
+            this.savebutton.TabIndex = 12;
+            this.savebutton.Text = "save";
+            this.savebutton.UseVisualStyleBackColor = false;
+            this.savebutton.Click += new System.EventHandler(this.savebutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(817, 597);
+            this.Controls.Add(this.savebutton);
+            this.Controls.Add(this.deletebutton);
+            this.Controls.Add(this.toDoListView);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.descriptTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("MS Reference Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.toDoListView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox descriptTextBox;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.DataGridView toDoListView;
+        private System.Windows.Forms.Button deletebutton;
+        private System.Windows.Forms.Button savebutton;
+    }
+}
+
---
+++ ToDoList/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string saveTasks = "ToDoList.txt";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toDoListView.Columns.Add("Задача", "Задача");
+            toDoListView.Columns.Add("Описание", "Описание");
+            toDoListView.Columns.Add("Время", "Время добавления");
+
+            DataGridViewCheckBoxColumn checkCol = new DataGridViewCheckBoxColumn();
+            checkCol.HeaderText = "Состояние";
+            toDoListView.Columns.Add(checkCol);
+
+            LoadData();
+            this.FormClosing += (s, args) => SaveData();
+        }
+
+        private void deletebutton_Click(object sender, EventArgs e)
+        {
+            toDoListView.Rows.Remove(toDoListView.CurrentRow);
+            SaveData();
+        }
+
+        private void savebutton_Click(object sender, EventArgs e)
+        {
+            toDoListView.Rows.Add(titleTextBox.Text, descriptTextBox.Text, DateTime.Now, false);
+            titleTextBox.Text = "";
+            descriptTextBox.Text = "";
+            SaveData();
+        }
+
+        private void SaveData()
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(saveTasks))
+                {
+                    foreach (DataGridViewRow row in toDoListView.Rows)
+                    {
+                        if (row.IsNewRow) continue; //пустые строки убирает
+
+                        string task = row.Cells[0].Value?.ToString() ?? ""; //если стоят пустые данные 
+                        string desc = row.Cells[1].Value?.ToString() ?? "";
+                        string time = row.Cells[2].Value?.ToString() ?? "";
+                        string state = (row.Cells[3].Value?.ToString() ?? "False");
+
+                        writer.WriteLine($"{task}|{desc}|{time}|{state}");
+                    }
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                foreach (string line in File.ReadAllLines(saveTasks)) //читает данные из файла
+                {
+                    string[] parts = line.Split('|');
+                    toDoListView.Rows.Add(parts[0], parts[1], parts[2], bool.Parse(parts[3]));
+                }
+            }
+            catch (Exception) { }
+        }
+    }
+}
